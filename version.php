8632c158a38f2a156f3037dba36b3a5298c32630
Manuel <manuel@wiris.com>
2017-09-06 09:19:20 +0200

    2017090500 Stable version

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2017090500;
-
+$plugin->release = '4.7.0.1374';
 $plugin->requires = 2012120300;
 $plugin->component = 'tinymce_tiny_mce_wiris';
 $plugin->dependencies = array ('filter_wiris' => 2017090500);
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
