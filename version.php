24415f18886840c747959bc9f371e38fcd7b37a7
Jenkins <jenkins@wiris.com>
2018-11-22 11:51:41 +0100

    2018112200 development version

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018111800;
+$plugin->version = 2018112200;
 
 $plugin->requires = 2012120300;
 $plugin->component = 'tinymce_tiny_mce_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2018111800);
+$plugin->dependencies = array ('filter_wiris' => 2018112200);
 $plugin->maturity = MATURITY_BETA;
