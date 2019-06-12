f242c7e877477000ae2492a7bc6ced9c3294c99b
Manuel Cagigas <manuel@wiris.com>
2019-06-12 12:54:36 +0200

    Release 7.14.0

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019051300;
-$plugin->release = '7.13.0';
+$plugin->version = 2019061200;
+$plugin->release = '7.14.0';
 $plugin->requires = 2012120300;
 $plugin->component = 'tinymce_tiny_mce_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2019051300);
-$plugin->maturity = MATURITY_BETA;
+$plugin->dependencies = array ('filter_wiris' => 2019061200);
+$plugin->maturity = MATURITY_STABLE;
