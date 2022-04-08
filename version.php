921fccabf29b543fefa35de757f46264df0e356a
Isaac Caparros Salva <isaac.caparros@wiris.com>
2022-04-08 11:44:25 +0200

    build(version): increase version for moodle 4 release

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021110900;
-$plugin->release = '7.27.1';
+$plugin->version = 2022040800;
+$plugin->release = '7.28.0';
 $plugin->requires = 2012120300;
 $plugin->component = 'tinymce_tiny_mce_wiris';
-$plugin->dependencies = array('filter_wiris' => 2021110900);
+$plugin->dependencies = array('filter_wiris' => 2022040800);
 $plugin->maturity = MATURITY_STABLE;
