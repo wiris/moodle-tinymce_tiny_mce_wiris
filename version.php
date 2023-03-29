c3c1f200f70c88ea558a847cd8052c51eb7dc947
carla-at-wiris <carla@wiris.com>
2023-03-29 11:12:16 +0200

    build(version): publish #8.1.0

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022112300;
-$plugin->release = '8.0.1';
+$plugin->version = 2023032900;
+$plugin->release = '8.1.0';
 $plugin->requires = 2012120300;
 $plugin->component = 'tinymce_tiny_mce_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2022112300);
+$plugin->dependencies = array ('filter_wiris' => 2023032900);
 $plugin->maturity = MATURITY_STABLE;
